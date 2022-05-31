eaa82dc240de8f7023000e085f2f493e9d2e1fc4
notAcoder <akblockchain15@gmail.com>
2022-05-31 07:32:48 +0530

    Inheritance edited

--- Inheritance.sol
+++ Inheritance.sol
@@ -2,66 +2,67 @@
 pragma solidity ^0.8.11;
 
 // Inheritance and Shadowing
+// Source - https://solidity-by-example.org/
 
 contract A {
     string public Contract = "A";
     event Str(string message);
 
     function str1() public virtual  {
-        emit Str("A.str1 is called");
+        emit Str("A.str1 is called");   // returns an event with message A.str1 is called 
     }
 
     function str2() public virtual  {
-        emit Str("A.str2 is called");
+        emit Str("A.str2 is called");   // returns an event with message A.str2 is called
     }    
 }
 
 contract B is A {
     constructor(){
-       Contract = "B";
+       Contract = "B";                   // overrides the variable Contract to B
     }
 
     function str1() public virtual override {
-        emit Str("B.str1 is called");
-        A.str1();
+        emit Str("B.str1 is called");   // returns an event with message B.str1 is called
+        A.str1();                      // returns an event with message A.str1 is called ; Calling directly;
     }
 
     function str2() public virtual override {
-        emit Str("B.str2 is called");
-        super.str2();
+        emit Str("B.str2 is called");   // returns an event with message B.str2 is called
+        super.str2();                   // returns an event with message A.str2 is called ; Calling on parent Contract with super
     }    
 
 }
 
 contract C is A {
     constructor(){
-       Contract = "C";
-    }
+       Contract = "C";                  // overrides the variable Contract to C
+    }   
 
     function str1() public virtual override {
-        emit Str("C.str1 is called");
-        A.str1();
+        emit Str("C.str1 is called");       // returns an event with message C.str1 is called
+        A.str1();                           // returns an event with message A.str1 is called ; Calling directly;
     }
 
     function str2() public virtual override {
-        emit Str("C.str2 is called");
-        super.str2();
+        emit Str("C.str2 is called");          // returns an event with message C.str2 is called
+        super.str2();                   // returns an event with message A.str2 is called ; Calling on parent Contract with super
     }    
 }
 
 contract D is B,C {
     constructor(){
-        Contract = "D";
+        Contract = "D";                 // overrides the variable Contract to C
     }
 
     function str1() public virtual override(B,C) {
-        emit Str("D.str1 is called");
-        super.str1();
+        emit Str("D.str1 is called");       // returns an event with message D.str1 is called
+        C.str1();                       // Call C; "C.st1 is called " --> "A.str1 is called";
     }
 
     function str2() public virtual override(B,C) {
-        emit Str("D.str2 is called");
-        super.str2();
+        emit Str("D.str2 is called");       // returns an event with message D.str2 is called
+        super.str2();                       // Call all parents; "C.str2 is called" --> "B.str2 is called" --> "A.str2 is called";git 
     }     
 
 }
